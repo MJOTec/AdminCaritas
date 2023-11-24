abb35790a6166962152e936221d44c657402922a
MJO <a00832807@tec.mx>
2023-11-24 14:02:52 -0600

    Funcion Shake
    Agregue la funcion para salir de la sesion al agitar el dispositivo.

--- AdminCaritas/AdminCaritas/ContentView.swift
+++ AdminCaritas/AdminCaritas/ContentView.swift
@@ -6,22 +6,39 @@
 //
 
 import SwiftUI
+import CoreMotion
 
 struct ContentView: View {
+    //Variable para acceder a los sensores
+    let motion = CMMotionManager()
+    @State private var accelerometerData: CMAccelerometerData?
+    @State private var accelerometerMoved = false
     var body: some View {
-        TabView{
-                Datos_View()
-                    .tabItem{
-                        Label("Menu",systemImage: "house.circle")
-                    }
-                Lista_Recolectores_View()
-                    .tabItem{
-                        Label("Recolectores", systemImage: "figure.walk.circle.fill")
-                    }
-                
+        NavigationStack{
+            VStack{
+                NavigationLink(destination: LogInView(), isActive: $accelerometerMoved) {
+                    EmptyView()
+                }
+                 
+                .hidden()
+                TabView{
+                    Datos_View()
+                        .tabItem{
+                            Label("Menu",systemImage: "house.circle")
+                        }
+                    Lista_Recolectores_View()
+                        .tabItem{
+                            Label("Recolectores", systemImage: "figure.walk.circle.fill")
+                        }
+                    
+                }
+                .tint(Color("PantoneAC"))
+                .navigationBarBackButtonHidden(true)
+                .onAppear(){
+                    startAccelerometer()
+                }
             }
-            .tint(Color("PantoneAC"))
-            .navigationBarBackButtonHidden(true)
+        }
             //sirve para cambiar el background del nav bar
             /*
              .onAppear(){
@@ -31,7 +48,27 @@ struct ContentView: View {
              */
             
     }
+    func startAccelerometer() {
+        if motion.isAccelerometerAvailable {
+            motion.accelerometerUpdateInterval = 0.5
+
+            motion.startAccelerometerUpdates(to: .main) { data, error in
+                guard let data = data else { return }
+                self.accelerometerData = data
+
+                // Aquí puedes realizar acciones adicionales según el movimiento del acelerómetro
+                if data.acceleration.x > 1.0 {
+                    // Si el dispositivo se mueve en la dirección positiva de X, por ejemplo, puedes navegar a otra pantalla
+                    self.accelerometerMoved = true
+                    print("\(data.acceleration.x)")
+                }
+            }
+        }
+    }
+
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
